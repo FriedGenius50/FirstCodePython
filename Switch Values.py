d2a7f58e3a359d471666482a64d6530084d62a63
FriedGenius50 <51935853+FriedGenius50@users.noreply.github.com>
2019-06-17 22:21:40 -0400

    Add files via upload

---
+++ Switch Values.py
@@ -0,0 +1,21 @@
+A=float (input("Please enter an integer for A: "))
+
+B=float (input("Please enter an integer for B: "))
+
+C=float
+
+print ("A= ",A)
+
+print ("B= ",B)
+
+if B<A:
+
+    C=A-B
+
+    print ("A-B=",C)
+
+   
+
+else:
+
+    print ("not possible")
